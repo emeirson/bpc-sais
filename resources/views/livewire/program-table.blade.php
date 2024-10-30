5b3e2769dff0357ea4385075021e4079029148ed
emeirson <calubemeirsonc@gmail.com>
2024-10-30 16:18:07 +0800

    Remove courses column in in program table

--- resources/views/livewire/program-table.blade.php
+++ resources/views/livewire/program-table.blade.php
@@ -108,12 +108,11 @@
     </div>
     <div class="overflow-x-auto border">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <x-table.thead :headers="['#', 'code', 'name', 'college', 'courses']" />
+            <x-table.thead :headers="['#', 'code', 'name', 'college']" />
             <tbody>
 
                 @forelse ($programs as $program)
                     <tr class="border-b dark:border-gray-700">
-
                         <x-table.data>
                             {{ $loop->iteration }}
                         </x-table.data>
@@ -126,24 +125,7 @@
                         <x-table.data>
                             {{ $program->college->description }}
                         </x-table.data>
-                        <x-table.data>
-                            <div class="flex items-center gap-x-2">
-                                @forelse ($program->courses as $course)
-                                    {{ $course->course_code }}
-                                @empty
-                                @endforelse
-                                <a href="{{ route('program-courses.edit', $program->id) }}">
-                                    <svg class="w-5 h-5 text-gray-600 hover:cursor-pointer dark:text-white"
-                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                                        height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                    </svg>
-                                </a>
-                            </div>
-                        </x-table.data>
-
+                        
                         <td class="px-4 py-3 flex items-center justify-end">
                             <button id="dropdown-button" data-dropdown-toggle="dropdown-{{ $loop->iteration }}"
                                 data-dropdown-placement="top"
