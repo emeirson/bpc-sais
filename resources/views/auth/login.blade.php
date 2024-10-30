e1baa7ff7ab67388d0c9561d8843aa9bcd336824
emeirson <calubemeirsonc@gmail.com>
2024-10-30 08:17:47 +0800

    Change login logo image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <img class="h-36 w-36" src="{{ asset('images/bpc-logo.png') }}" alt="BPC Logo">
         </x-slot>
 
         <x-validation-errors class="mb-4" />
